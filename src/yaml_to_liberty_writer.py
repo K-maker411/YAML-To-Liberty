9eace69b1bcfe9366aff03f1a22355a4ffe2ed21
Kaushik Balantrapu <60593207+K-maker411@users.noreply.github.com>
2024-05-30 23:34:57 +0000

    Started setting up yaml to lib writer, added "classes" of attributes for liberty file in separate files

--- src/yaml_to_liberty_writer.py
+++ src/yaml_to_liberty_writer.py
@@ -1,2 +1,35 @@
+import yaml
+import numbers
+
 class YamlToLibertyWriter:
-  pass
+  def __init__(self, yaml_file_read_stream):
+    self.yaml_file = yaml.safe_load(yaml_file_read_stream)
+    
+  # ints, floats, or strings are simple because they're non-nested
+  # TODO - is this bad design? Probably not a huge deal, but worth checking
+  def is_simple_attribute(self, attr):
+    return isinstance(attr, (numbers.Number, str))
+
+  # list and dicts are complex
+  # TODO - is this bad design? Probably not a huge deal, but worth checking
+  def is_complex_attribute(self, attr):
+    return isinstance(attr,  (list, dict))
+  
+  # "simple" is defined as non-list/dictionary/nested structure
+  def get_simple_library_level_attributes(self):
+    simple_attrs_string = ""
+    for attr in self.yaml_file.get("library"):
+      if self.is_simple_attribute(attr):
+        # TODO - add code for this later
+        pass
+      else:
+        return simple_attrs_string
+
+  def get_full_library(self):
+    full_lib = ""
+    full_lib += "library(" + self.yaml_file.get("library").get("name") + ") {"
+    # add the rest of the stuff here (in a while, crocodile)
+    full_lib += "\n"
+    return full_lib
+    
+    
--- tests/test_yaml_to_liberty_writer.py
+++ tests/test_yaml_to_liberty_writer.py
@@ -1,1 +1,5 @@
 import pytest
+from src.yaml_to_liberty_writer import YamlToLibertyWriter
+
+def test_write_simple_lib_level_attributes():
+  pass
