640a9f508e4009ca7ce69a009dc2a0c687afbe5d
K-maker411 <chiefkash2004@gmail.com>
2024-06-02 23:28:12 -0700

    Added json support to attributes_provider

--- src/attributes_provider.py
+++ src/attributes_provider.py
@@ -1,11 +1,13 @@
-# TODO - this class doesn't need to exist separately - including it as part of YamlParser would make 
-# more sense and keep the code simpler
+import json
+
+# TODO - this class may need to stay as-is because it will provide the functionality of handling JSON with the type specifications
 class AttributesProvider:
   def __init__(self, library_level_simple_attributes, library_level_scaling_attributes, library_level_default_attributes):
-    # Current idea: these will be individual sets, since the order will be based on what the YAML has
-    self.library_level_simple_attributes = {line.strip() for line in library_level_simple_attributes}
-    self.library_level_scaling_attributes = {line.strip() for line in library_level_scaling_attributes}
-    self.library_level_default_attributes = {line.strip() for line in library_level_default_attributes}
+    # Current idea: pull these from the individual 
+    self.library_level_simple_attributes = json.load(library_level_simple_attributes)
+    self.library_level_default_attributes = json.load(library_level_default_attributes)
+    self.library_level_scaling_attributes = json.load(library_level_scaling_attributes)
+    
 
   def get_library_level_simple_attributes(self):
     return self.library_level_simple_attributes
