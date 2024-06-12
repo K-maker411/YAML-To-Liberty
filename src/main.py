620662483b3fb9439595ac388a4abe801380d4ef
Kaushik Balantrapu <60593207+K-maker411@users.noreply.github.com>
2024-06-12 20:37:27 +0000

    Added shebang to main

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import argparse
 from .yaml_to_liberty_writer import YamlToLibertyWriter
 from .attributes_provider import AttributesProvider
--- src/yaml_to_liberty_writer.py
+++ src/yaml_to_liberty_writer.py
@@ -38,31 +38,6 @@ class YamlToLibertyWriter:
 
     # UPDATE: for now, every value can be a string, so we can just return the string with double quotes
     return self.get_string_attr_as_string(attr, dict_containing_attr)
-    '''
-      # if the value at a given key is a list, that means the attribute can be one of several values (but NOT types)
-      if isinstance(level_dict.get(attr), list):
-        # for these, we can just return the attr value without quotes on either side
-        return self.get_attr_as_string_basic(attr, dict_containing_attr)
-      # if the value at a given key is a dict, that means the attribute has at least one possible type (but possibly more, e.g. name and name_list)
-      elif isinstance(level_dict.get(attr), dict):
-        # get list of types
-        type_of_attr_in_level = level_dict.get(attr).get("type")
-        # if no quotes or anything are required to be added, just return the value with the attribute name is the specified format
-        if ("float" in type_of_attr_in_level
-            or "trip_point_value" in type_of_attr_in_level
-            or type_of_attr_in_level == [True, False]
-            or "valueenum" in type_of_attr_in_level):
-          return self.get_attr_as_string_basic(attr, dict_containing_attr)
-  
-        # TODO - edit this later, for now everything that's not a float (or float-like) on lib-level will be string
-        else:
-          return self.get_string_attr_as_string(attr, dict_containing_attr)
-  
-      # this case should never be called unless there's an error in the YAML - if so, just don't add anything to the string outside,
-      # so return empty string here
-      else:
-        return ""
-    '''
 
   # TODO - add support for this once initial cell and pin stuff is done
   # bus definition
@@ -261,14 +236,3 @@ class YamlToLibertyWriter:
     # TODO - add more groups here as necessary, doesn't matter for now
     
     return self.remove_blank_lines(self.get_function_notation_string("library", self.yaml_file.get("library").get("name"), full_lib, 0))
-    
-    #for attr in self.yaml_file.get("library"):
-      #if attr == constants_yaml_to_liberty_writer.CELL:
-        #full_lib += self.get_all_cells_in_library_as_string()
-
-
-    
-    full_lib += "library(" + self.yaml_file.get("library").get("name") + ") {"
-    # add the rest of the stuff here (in a while, crocodile)
-    full_lib += "\n"
-    return full_lib
